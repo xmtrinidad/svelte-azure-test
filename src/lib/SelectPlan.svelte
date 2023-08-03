230312d74ff8e8a1f0c9c29fdb577dad1f20598b
Xavier <xmtrinidad@gmail.com>
2023-08-02 22:57:23 -0700

    updated some styles

--- src/lib/SelectPlan.svelte
+++ src/lib/SelectPlan.svelte
@@ -1,31 +1,46 @@
 <script>
   import FormHeader from "./FormHeader.svelte";
+  import ArcadeIcon from "./icons/ArcadeIcon.svelte";
+    import BusinessIcon from "./icons/BusinessIcon.svelte";
+    import ShieldIcon from "./icons/ShieldIcon.svelte";
 
-  const plans = [
+  let plans = [
     {
       name: "Arcade",
       price: "$9/mo",
-      icon: "arcade",
+      icon: ArcadeIcon,
+      active: true
     },
     {
       name: "Pro",
       price: "$19/mo",
-      icon: "pro",
+      icon: ShieldIcon,
+      active: false
     },
     {
       name: "Business",
       price: "$49/mo",
-      icon: "business",
+      icon: BusinessIcon,
+      active: false
     },
   ];
+
+  function onPlanClick(plan) {
+    plans.forEach(p => p.active = false);
+    plan.active = true;
+
+    plans = plans;
+  }
 </script>
 
 <div class="personal-info">
   <FormHeader header="Select your plan" text="You have the option of monthly or yearly billing"></FormHeader>
   <form action="">
     {#each plans as plan}
-      <button class="price-container">
-        <div class="icon">{plan.icon}</div>
+      <button on:click={() => onPlanClick(plan)} class="price-container {plan.active ? 'active' : ''}">
+        <div class="icon-container">
+          <svelte:component this={plan.icon} />
+        </div>
         <div class="price-name-container">
           <span class="price-name">{plan.name}</span>
           <span class="price">{plan.price}</span>
@@ -38,6 +53,10 @@
 
 <style>
 
+.personal-info :global(svg) {
+  fill: #fff;
+}
+
 form {
   margin: 24px 0;
   display: flex;
@@ -46,7 +65,7 @@ form {
 
 .price-container {
   border: none;
-  background-color: #f8f9fe;
+  background-color: #fff;
   border: 1px solid #5a529e;
   width: 132px;
   height: 140px;
@@ -57,6 +76,31 @@ form {
   border-radius: 8px;
   cursor: pointer;
   text-align: left;
+}
+
+.price-container.active {
+  background-color: #f8f9fe;
+}
+
+.icon-container {
+  width: 36px;
+  height: 36px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100%;
+}
+
+.price-container:first-child .icon-container {
+  background-color: #ffab7c;
+}
+
+.price-container:nth-child(2) .icon-container {
+  background-color: #ff7b89;
+}
+
+.price-container:last-child .icon-container {
+  background-color: #4341ff;
 }
 
 .price-name-container {
